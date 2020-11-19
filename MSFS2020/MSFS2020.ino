f4f9f73e237189a52aff7083805c8becab94cc18
Diogo Dias <XdiiasXD@users.noreply.github.com>
2020-11-19 17:49:30 +0000

    Create MSFS2020.ino

---
+++ MSFS2020/MSFS2020.ino
@@ -0,0 +1,43 @@
+//Including a library
+#include <Gamepad.h>
+#include <Encoder.h>
+
+
+//Initializing a Gamepad
+Gamepad gp;
+
+Encoder altitude(5, 6);
+
+int button1 = 0;
+int button2 = 1;
+
+void setup() {
+
+long right = -999;
+
+}
+
+void loop() {
+  
+  long newright;
+  newright = altitude.read();
+
+  if (newright > right){
+
+      gp.setButtonState(button1, true);
+      delay(1);
+      gp.setButtonState(button1,false);
+      
+  }
+  if (right > newright){
+      
+      gp.setButtonState(button2, true);
+      delay(1);
+      gp.setButtonState(button2, false);
+
+      newright = right
+    
+  }
+  
+
+}
