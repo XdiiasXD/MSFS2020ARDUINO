56c497e9296e29371135fa104c193b514491ca45
Diogo Dias <XdiiasXD@users.noreply.github.com>
2020-11-19 19:01:14 +0000

    Update MSFS2020.ino

--- MSFS2020/MSFS2020.ino
+++ MSFS2020/MSFS2020.ino
@@ -11,11 +11,13 @@ Encoder altitude(5, 6);
 //Setting gamepad buttons to windows
 int button1 = 0;
 int button2 = 1;
+int button3 = 2;
 
 void setup() {
 //Pin modes
-pinMode(5, INPUT);
-pinMode(6, INPUT);
+pinMode(7, INPUT);
+pinMode(8, INPUT);
+pinMode(9, INPUT);
 
 long right = -999;
 
@@ -23,20 +25,32 @@ long right = -999;
 
 void loop() {
   
+  if (9 == true)
+	  
+	gp.setButtonState(button3, true);
+	
+	else 
+		
+	gp.setButtonState(button3; false);
+  
   long newright;
   newright = altitude.read();
 
   if (newright > right){
 
       gp.setButtonState(button1, true);
-      delay(1);
-      gp.setButtonState(button1,false);
+    
+	else
+      
+	  gp.setButtonState(button1,false);
       
   }
   if (right > newright){
       
       gp.setButtonState(button2, true);
-      delay(1);
+     
+	 else
+		 
       gp.setButtonState(button2, false);
 
       newright = right
